61b50b90c397c75cc8f2dc648b7f2d7a056eec8b
Eslam Genedy <eslamgenedy377@yahoo.com>
2019-01-04 18:01:34 +0200

    282B-Div2

--- Codeforces/Solution/282B-Div2.cpp
+++ Codeforces/Solution/282B-Div2.cpp
@@ -1,6 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std ; 
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
-    return 0 ;
+    int x,c=0,y,z;
+    string s="";
+    cin>>x;
+    while(x--)
+    {
+        cin>>y>>z;
+        if(c+y>500)
+        {
+            c-=z;
+            s+="G";
+        }
+        else
+        {
+           c+=y;
+           s+="A";
+        }
+        if (c>500||c<-500) {
+            cout<<-1;
+            break;
+        }
+    }
+    cout<<s;
 }
