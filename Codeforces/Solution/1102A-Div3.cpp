80d9ce39788a113940b16fa862503935774271d8
Eslam Genedy <eslamgenedy377@yahoo.com>
2019-01-10 07:34:21 +0200

    1102-Div3.cpp After Hacking

--- Codeforces/Solution/1102A-Div3.cpp
+++ Codeforces/Solution/1102A-Div3.cpp
@@ -1,36 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std ;
+// #include <bits/stdc++.h>
+// using namespace std ;
+// int main()
+// {
+//     int n ; 
+//     cin >> n;  
+//     // float to = 2
+//     int to = 2 ; 
+//     int sum = 0 ; 
+//     int arr[n]  ; 
+//     int ans[20] = {1,3,6,10,5,11,8,16,15,5,6,8,11,5,10,6,13,11,10,0};
+//     // for(int i =0 ; i < n ; i++)
+//     // {
+//     //     arr[i] = i+1 ;
+//     //     sum = (sum%10) +(arr[i]%10) ; 
+//     //     cout <<sum<<endl ;
+//     // }
+//     if(n>20)
+//     {
+//         n = n % 20 ;
+//         n = ans[n-1] ; 
+
+//     }
+//     else{
+//         n = ans[n-1] ; 
+//     }
+//     // cout<<"n "<<n<<" * "<<endl;
+//     if (n % 2 == 0 )
+//         {
+//             cout << "0" ;
+//         }
+
+//     else 
+//         cout << "1" ;
+//     return 0 ;
+// }
+#include <iostream>
+using namespace std;
+ 
 int main()
 {
-    int n ; 
-    cin >> n;  
-    // float to = 2
-    int to = 2 ; 
-    int sum = 0 ; 
-    int arr[n]  ; 
-    int ans[20] = {1,3,6,10,5,11,8,16,15,5,6,8,11,5,10,6,13,11,10,0};
-    // for(int i =0 ; i < n ; i++)
-    // {
-    //     arr[i] = i+1 ;
-    //     sum = (sum%10) +(arr[i]%10) ; 
-    //     cout <<sum<<endl ;
-    // }
-    if(n>20)
-    {
-        n = n % 20 ;
-        n = ans[n-1] ; 
-
-    }
-    else{
-        n = ans[n-1] ; 
-    }
-    // cout<<"n "<<n<<" * "<<endl;
-    if (n % 2 == 0 )
-        {
-            cout << "0" ;
-        }
-
-    else 
-        cout << "1" ;
-    return 0 ;
+	long long a,b;
+	cin>>a;
+	cout<< ((a*(a+1)/2)%2 ? 1 : 0);
+	return 0;
 }
